e609fd2ea0d2f0824c8a68b418bb982b8c203f63
Johan Perez <johan.perez05@gmail.com>
2023-07-17 23:22:13 +0200

    footer and main changed

--- assets/react/controllers/Footer.jsx
+++ assets/react/controllers/Footer.jsx
@@ -1,105 +1,76 @@
 import React from 'react';
 
 const Footer = (props) => {
-    const userId = props.userId;
+  const userId = props.userId;
   return (
     <footer className="bg-dark text-white pt-5 pb-4">
-
-        <div className="container text-center text-md-left">
-
-            <div className="row text-center text-md-left">
-
-                <div className="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
-
-                    <h5 className='text-uppercase mb-4 font-weight-bold text-warning'>Compagny Name</h5>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus sequi, asperiores dolore expedita nostrum!</p>
-
-                </div>
-
-                <div className='col-md-2 col-lg-2 col-xl-2 mx-auto mt-3'>
-
-                    <h5 className='text-uppercase mb-4 font-weight-bold text-warning'>Products</h5>
-                <p>
-                <a href="#" className='text-white' style={{ textDecoration: 'none' }}>TheProviders</a>
-                </p>
-                <p>
-                <a href="#" className='text-white' style={{ textDecoration: 'none' }}>Creativity</a>
-                </p>
-                <p>
-                <a href="#" className='text-white' style={{ textDecoration: 'none' }}>SourceFiles</a>
-                </p>
-                <p>
-                <a href="#" className='text-white' style={{ textDecoration: 'none' }}>Bootstrap 5 alpha</a>
-                </p>
-                </div>
-
-                <div className='col-md-3 col-lg-2 col-xl-2 mx-auto mt-3'>
-                    <h5 className='text-uppercase mb-4 font-weight-bold text-warning'>Useful links</h5>
-                    <p>
-                <a href="#" className='text-white' style={{ textDecoration: 'none' }}>Your Account</a>
-                </p>
-                <p>
-                <a href="#" className='text-white' style={{ textDecoration: 'none' }}>Become an Affiliate</a>
-                </p>
-                <p>
-                <a href="#" className='text-white' style={{ textDecoration: 'none' }}>Shipping Rates</a>
-                </p>
-                <p>
-                <a href="#" className='text-white' style={{ textDecoration: 'none' }}>Help</a>
-                </p>
-                </div>
-
-                <div className='col-md-4 col-lg-3 col-xl-3 mx-auto mt-3'>
-                    <h5 className='text-uppercase mb-4 font-weight-bold text-warning'>Contact</h5>
-                    <p>
-                        <i className='fas fa-home mr-3'></i>New York, NY 2333, US
-                    </p>
-                    <p>
-                        <i className='fas fa-enveloppe mr-3'></i>theproviders98@gmail.com
-                    </p>
-                    <p>
-                        <i className='fas fa-phone mr-3'></i>0123456789
-                    </p>
-                    <p>
-                        <i className='fas fa-print mr-3'></i>0123456789
-                    </p>
-                </div>
-
+      <div className="container text-center text-md-left">
+        <div className="row text-center text-md-left">
+          <div className="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+            <a className="footer-brand text-uppercase mb-4 font-weight-bold text-warning" href="#">
+              Garage V.Parrot
+            </a>
+            <div className="mt-5">
+              <img src="assets/images/logo parrot.png" alt="logo du garage" />
             </div>
+          </div>
+          <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+            <h5 className="text-uppercase mb-4 font-weight-bold text-warning">Nos horaires</h5>
+            <p className='text-white'>
+              Lundi - vendredi
+              <br/>
+              8h45 - 12h / 14h - 18h
+            </p>
+            <p className='text-white'>
+              Samedi
+              <br/>
+              8h45 - 12h
+            </p>
+            
+          </div>
+          <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
+            <h5 className="text-uppercase mb-4 font-weight-bold text-warning">Nous contacter</h5>
+            <p>
+              <i className="fas fa-home mr-3"></i>123 Rue des mécaniciens<br/>31000 Toulouse,<br/> France
+            </p>
+            <p>
+              <i className="fas fa-envelope mr-3"></i>contact@garagevparrot.fr
+            </p>
+            <p>
+              <i className="fas fa-phone mr-3"></i>0123456789
+            </p>
 
-            <hr className='mb-4' />
-
-            <div className='row align-items-center'>
-
-                <div className='col-md-7 col_lg-8'>
-                    <p> Copyright @2020 All rights reserved by:
-                        <a href="#" style={{ textDecoration: 'none' }}>
-                            <strong className='text-warning'>The Providers</strong>
-                        </a>
-                    </p>
-
-                </div>
-
-                <div className='col-md-5 co-lg-4'>
-                    <div className='text-center text-md-right'>
-
-                        <ul className='list-unstyled list-inline'>
-                            <li className='list-inline-item'>
-                                <a href="#" className='btn-floating btn-sm text-white' style={{ fontSize: '23px' }}><i className='fab fa-facebook'></i></a>
-                            </li>
-                            <li className='list-inline-item'>
-                                <a href="#" className='btn-floating btn-sm text-white' style={{ fontSize: '23px' }}><i className='fab fa-instagram'></i></a>
-                            </li>
-                        </ul>
-
-                    </div>
-
-                </div>
-
-            </div>
-
+          </div>
+          <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
+            <h5 className="text-uppercase mb-4 font-weight-bold text-warning">Nous suivre</h5>
+            <ul className="list-unstyled list-inline">
+              <li className="list-inline-item">
+                <a href="#" className="btn-floating btn-sm text-white" style={{ fontSize: '23px' }}>
+                  <i className="fab fa-facebook">f</i>
+                </a>
+              </li>
+              <li className="list-inline-item">
+                <a href="#" className="btn-floating btn-sm text-white" style={{ fontSize: '23px' }}>
+                  <i className="fab fa-instagram">i</i>
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
-
+        <hr className="mb-4" />
+        <div className="row align-items-center">
+          <div className="col-md-7 col-lg-8">
+            <p>
+              {' '}
+              Copyright @2023
+              <a href="#" style={{ textDecoration: 'none' }}>
+                <strong className="text-white"> Mentions légales </strong>
+              </a>
+              | site réalisé par Perez Johan
+            </p>
+          </div>
+        </div>
+      </div>
     </footer>
   );
 };
--- assets/react/controllers/Main.jsx
+++ assets/react/controllers/Main.jsx
@@ -1,17 +1,100 @@
 import React from 'react';
 
 const Main = (props) => {
-
-    const fullName = "Hello World";
+  const fullName = "Hello World";
   return (
-    <div className="text-danger">
-      Hello {fullName}
-      <div>
-        <button className="btn btn-primary">Cliquez ici</button>
-      </div>
-    </div>
+    <main>
+      <section className='repairs mt-5'>
+        <div className="card mb-3 w-100">
+          <div className="row g-0">
+            <div className="col-md-4">
+              <img src="#" className="img-fluid rounded-start" alt="image repairs" />
+            </div>
+            <div className="col-md-8 text-center">
+              <div className="card-body">
+                <h5 className="card-title text-dark text-uppercase">Réparations</h5>
+                <p className="card-text">Réparez en toute confiance. Notre garage automobile met à votre disposition une équipe de mécaniciens expérimentés et qualifiés, prêts à prendre soin de votre voiture.</p>
+                <button type="button" className="btn btn-info">En savoir plus</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className='maintenance'>
+        <div className="card mb-3 w-100">
+            <div className="row g-0">
+              
+              <div className="col-md-8 text-center">
+                <div className="card-body">
+                  <h5 className="card-title text-dark text-uppercase">Entretiens</h5>
+                  <p className="card-text">Chez Garage V.Parrot, nous comprenons l'importance d'un entretien régulier pour prolonger la durée de vie de votre véhicule. Nos techniciens compétents vous offrent des services d'entretien complets</p>
+                  <button type="button" className="btn btn-info">En savoir plus</button>
+                </div>
+              </div>
+              <div className="col-md-4">
+                <img src="#" className="img-fluid rounded-start" alt="image repairs" />
+              </div>
+            </div>
+          </div>
+      </section>
+
+      <section className='second-hand'>
+        <div className="card mb-3 w-100">
+            <div className="row g-0">
+              <div className="col-md-4">
+                <img src="#" className="img-fluid rounded-start" alt="image repairs" />
+              </div>
+              <div className="col-md-8 text-center">
+                <div className="card-body">
+                  <h5 className="card-title text-dark text-uppercase">Ventes d'occasion</h5>
+                  <p className="card-text">Roulez avec style et fiabilité. Découvrez notre sélection exclusive de voitures d'occasion de qualité supérieure, prêtes à prendre la route.</p>
+                  <button type="button" className="btn btn-info">En savoir plus</button>
+                </div>
+              </div>
+            </div>
+          </div>
+      </section>
+
+      <section className='container contact-us w-100 text-center my-5'>
+        <div>
+        Contactez-nous pour toute demande au <strong>01 23 45 67 89</strong> ou par mail <strong>contact@garagevparrot.fr</strong>
+        </div>
+        <div>
+        <button type="button" className="btn btn-info">Contacter-nous</button>
+        </div>
+      </section>
+    
+      <hr />
+
+      <section className='container comments my-5 text-center'>
+        <h2 className='uppercase font-weight-bold mb-5'>Témoignages</h2>
+        <blockquote className="blockquote mb-0">
+          <p><em>"Le garage automobile idéal ! Un service impeccable et des voitures d'occasion de qualité. Je suis entièrement satisfait de mon expérience avec eux."</em></p>
+          <div className="blockquote-footer text-end pe-5 me-5">Marie D.</div>
+        </blockquote>
+
+      </section>
+
+      <hr />
+
+      <section className='container comment-form w-50 mb-5'>
+        <h2 className='text-center'>Votre avis</h2>
+        <div className="mb-3">
+          <input type="name" className="form-control" id="exampleFormControlInput1" placeholder="Nom" />
+        </div>
+        <div className="mb-3">
+          <textarea className="form-control" id="exampleFormControlTextarea1" rows="3" placeholder='Votre avis...'></textarea>
+        </div>
+        <div className="mb-3">
+          <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="Email" />
+        </div>
+        <div className="text-center">
+          <button type="submit" className="btn btn-primary w-100">Envoyer</button>
+        </div>
+        
+      </section>
+    </main>
   );
 };
 
 export default Main;
-
